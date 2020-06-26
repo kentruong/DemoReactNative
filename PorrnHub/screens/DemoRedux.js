0544497d482a2f3b3ce94679d75846cbf3d26b82
KenTruong <anhkhoaprotruong@gmail.com>
2020-06-26 17:14:55 +0700

    a
    a

---
+++ PorrnHub/screens/DemoRedux.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class DemoRedux extends Component {
+	render(){
+		return(
+			// State nơi lưu trữ tất cả các biến trong ứng dụng
+			// Action là các dự kiện của người dùng tương tác trên UI
+			// Reducer
+			// Store 
+			<View>
+
+				<Text>DemoRedux</Text>
+			</View>
+		)
+	}
+}
